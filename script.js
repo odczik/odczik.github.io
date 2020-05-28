3e6bd448dd5cc68130a1588c7081291e08776d2f
odczik <58082541+odczik@users.noreply.github.com>
2020-05-28 14:35:41 +0200

    Website

---
+++ script.js
@@ -0,0 +1,13 @@
+const ubetr0nImg = document.getElementById("ubetr0n-img")
+const snakeImg = document.getElementById("snake-img")
+const webhookImg = document.getElementById("webhook-img")
+
+ubetr0nImg.onclick = function(){
+    window.open("https://home.ubetr0n.tk","_self")
+}
+snakeImg.onclick = function(){
+    window.open("https://snake.odczik.tk","_self")
+}
+webhookImg.onclick = function(){
+    window.open("https://webhook.odczik.tk","_self")
+}
